40b214c94d75d3435b86c9a61bd547118ef611b1
Gabriel Ayres <gbayres@hotmail.com>
2020-09-23 13:56:28 -0300

    Slices

---
+++ Standard Library/#4 - Slicing lists.py
@@ -0,0 +1,6 @@
+def chunks(lst, n):
+    for i in range(0, len(lst), n):
+        yield lst[i:i+n] #Let's turn this to a generator
+
+my_list = [2,1,3,4,3,2,3,4,2,3,2,5,3,2]
+print(list(chunks(my_list,3)))
