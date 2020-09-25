583e01a91cb5433123a4da39994ce56009568354
Gabriel Ayres <gbayres@hotmail.com>
2020-09-25 19:02:23 -0300

    OOP

--- Standard Library/#4 - Slicing lists.py
+++ Standard Library/#4 - Slicing lists.py
@@ -4,3 +4,9 @@ def chunks(lst, n):
 
 my_list = [2,1,3,4,3,2,3,4,2,3,2,5,3,2]
 print(list(chunks(my_list,3)))
+
+
+#======Short form==========
+
+def slice_list(lst, n):
+    return [lst[i:i+n] for i in range(0, len(lst), n)]
